14a2b27fe5d078feccd515068ed3ff581b3610dd
Chubbi-Stephen <udeokoliestephen@gmail.com>
2026-04-20 11:16:34 +0100

    complete global app configuration and design systems

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Standard layout and central router configuration
 function App() {
-	return (
-		<>
-			<h1 className="text-center">Splitt</h1>
-		</>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"  />
+        
+        <Route path="*" element={
+          <div className="flex min-h-screen items-center justify-center bg-brand-bg text-brand-navy">
+            Page Not Found (404)
+          </div>
+        } />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
